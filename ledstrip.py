b9bbebd45e0a651ee889ca3fc36c31737ea9428a
Ocean <marloscott62326@gmail.com>
2018-07-05 21:51:35 +1200

    Added intensity to LED list tuple, this allows color to be changed while maintaining pattern.

--- ledstrip.py
+++ ledstrip.py
@@ -16,50 +16,56 @@ class LedStrip:
         led_color:      Stores current color value for all LEDs
         led_pos:        Used for tracking a single LED position
         led_driver:     Instance of the ws2812 Driver
+        led_intensity:  Sets main LEDs intensity (value between 0-1)
     """
 
     def __init__(self, led_number=1, init_color=(0xff,0xff,0xff), spi_bus=1, intensity=1):
         self.led_number = led_number
         self.led_color  = init_color
-        self.led_list   = [self.led_color for x in range(led_number)]
-        self.led_driver = ws2812.ws2812(spi_bus=spi_bus, led_count=led_number, intensity=intensity)
+        self.led_list   = [(self.led_color, intensity) for i in range(led_number)]
+        self.led_driver = ws2812.ws2812(spi_bus=spi_bus, led_count=led_number, intensity=1)
         self.led_pos    = 0
-        self.led_n_color    = 0
+        self.led_intensity = intensity
+        self.led_n_color    = 107
         self.update()
 
 
     def update(self):
-        self.led_driver.show(self.led_list)
+        stripped_led_list = [self.led_list[x][0] for x in range(self.led_number)]
+        self.led_driver.show(stripped_led_list)
 
 
     def init_kit_scroll(self):
-        self.led_list = [(self.led_color[0]>>(1*x),
-                        self.led_color[1]>>(1*x),
-                        self.led_color[2]>>(1*x))
-                        for x in range(self.led_number)]
+        fade_amount = self.led_intensity / (self.led_number*0.6)    # This will make 60% of LEDs be on for the effect
+        for x in range(self.led_number):
+            self.set_led_intensity(x, self.led_color, self.led_intensity - fade_amount*x)
         self.led_pos = 0
         self.update()
 
 
     def init_single_led(self):
-        self.led_list = [(0,0,0) for i in range(self.led_number)]
-        self.led_list[0] = self.led_color
+        self.led_list = [((0,0,0),0) for i in range(self.led_number)]
+        self.led_list[0] = (self.led_color,self.led_intensity)
         self.led_pos = 0
         self.update()
 
 
     def shift_right(self):
         if(self.led_pos<self.led_number-1):
-            self.led_list = [self.led_list[i-1] for i in range(self.led_number)]
+            self.led_list = [self.led_list[i-1]
+                            for i in range(self.led_number)]
             self.led_pos += 1
             self.update()
 
 
     def shift_left(self):
         if(self.led_pos>0):
-            self.led_list =[self.led_list[(i-self.led_number)+1] for i in range(self.led_number)]
+            self.led_list =[self.led_list[(i-self.led_number)+1]
+                           for i in range(self.led_number)]
             self.led_pos -= 1
             self.update()
+        else:
+
 
 
     def swap(self, led1, led2):
@@ -96,12 +102,22 @@ class LedStrip:
              self.shift_left_swap()
              pyb.delay(delay)
 
+    def set_led_intensity(self, led, color, intensity):
+        if(intensity < 0 or intensity > 1):
+            self.led_list[led] = ((0,0,0),0)
+        else:
+            r = (int)(color[0]*intensity)
+            g = (int)(color[1]*intensity)
+            b = (int)(color[2]*intensity)
+            self.led_list[led] = ((r,g,b),intensity)
+        self.update()
+
     def change_color(self):
         r = int((1 + math.sin(self.led_n_color * 0.1324)) * 127)
         g = int((1 + math.sin(self.led_n_color * 0.1654)) * 127)
         b = int((1 + math.sin(self.led_n_color * 0.1)) * 127)
         self.led_color = (r,g,b)
         for x in range(self.led_number):
-            self.led_list[x] = self.led_color
+            self.set_led_intensity(x, self.led_color, self.led_list[x][1])
         self.led_n_color += 1
         self.update()
